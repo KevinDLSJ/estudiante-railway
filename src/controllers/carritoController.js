b025ddabd514b679968ee9d9121be66f1c1e2522
Kevin De Los Santos <21301061550104@cetis155.edu.mx>
2023-05-28 16:41:09 -0600

    7

--- src/controllers/carritoController.js
+++ src/controllers/carritoController.js
@@ -113,6 +113,7 @@ function recp(req,res) {
       req.getConnection((err, conn) => {
         //selecciona la tabla de carrito
         conn.query('SELECT a.folio,a.fecha,a.status,a.corre_emp,a.correo_clie,b.cantidad,b.precio,c.name FROM pedido a,detalle b, product c WHERE a.folio = ? AND a.folio = b.folio AND b.id_producto = c.id_producto',[id],(err,ped)=>{
+          console.log(ped)
           if(err) throw err
             req.getConnection((err,conn) => {
               conn.query('SELECT SUM(cantidad*precio) FROM detalle WHERE folio =?',[id],(err,tota) =>{
