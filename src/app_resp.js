6f6816ea1a3987acef146f0c2f387cf81829de1f
Kevin De Los Santos <21301061550104@cetis155.edu.mx>
2023-05-30 20:43:16 -0600

    15

--- src/app_resp.js
+++ src/app_resp.js
@@ -4,8 +4,8 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:3000',
-  clientID: 'UeXMJQBGZXze7DgWvNBt7sOvpYjd2Fw9',
+  baseURL: 'https://estudiante-cafetery.up.railway.app/',
+  clientID: 'aP0glnG88Bhj49aztGJqLCRpGxjZN7OF',
   issuerBaseURL: 'https://metacafetery.us.auth0.com'
 };
 
