fd3fa120ccbf229f8ce6abd5f69ae9590b15c38f
Poyodulce17 <kevinssj987@gmail.com>
2023-06-08 22:28:18 -0600

    16

--- src/app_resp.js
+++ src/app_resp.js
@@ -6,7 +6,7 @@ const config = {
   secret: 'a long, randomly-generated string stored in env',
   baseURL: 'https://estudiante-cafetery.up.railway.app/',
   clientID: 'aP0glnG88Bhj49aztGJqLCRpGxjZN7OF',
-  issuerBaseURL: 'https://metacafetery.us.auth0.com'
+  issuerBaseURL: 'https://metacafetery.us.auth0.com/'
 };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
